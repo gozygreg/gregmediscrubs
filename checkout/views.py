2374954e8d8a60cffb766c5a70e3387bbdcef0c3
gozygreg <goziennabuife@yahoo.com>
2023-03-20 14:29:35 +0000

    Add functionality to profile app by implementing order history view and template

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from store.models import Product
 from bag.contexts import bag_contents
 
@@ -71,6 +73,7 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
+            # Save user profile information
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
@@ -91,7 +94,25 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        # Add user profile details to form
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -105,11 +126,72 @@ def checkout(request):
     return render(request, 'checkout/checkout.html', context)
 
 
-def checkout_success(request, order_number):
-    """ For successful checkouts """
+# def checkout_success(request, order_number):
+#     """ For successful checkouts """
 
+#     save_info = request.session.get('save_info')
+#     order = get_object_or_404(Order, order_number=order_number)
+
+#     if request.user.is_authenticated:
+#         profile = UserProfile.objects.get(user=request.user)
+#         # Attach the user's profile to the order
+#         order.user_profile = profile
+#         order.save()
+
+#         # Save the user's info
+#         if save_info:
+#             profile_data = {
+#                 'default_phone_number': order.phone_number,
+#                 'default_country': order.country,
+#                 'default_postcode': order.postcode,
+#                 'default_town_or_city': order.town_or_city,
+#                 'default_street_address1': order.street_address1,
+#                 'default_street_address2': order.street_address2,
+#                 'default_county': order.county,
+#             }
+#             user_profile_form = UserProfileForm(profile_data, instance=profile)
+#             if user_profile_form.is_valid():
+#                 user_profile_form.save()
+
+#     messages.success(request, f'Order successfully processed! \
+#         Your order number is {order_number}. A confirmation \
+#         email will be sent to {order.email}.')
+
+#     if 'bag' in request.session:
+#         del request.session['bag']
+
+#     context = {'order': order}
+
+#     return render(request, 'checkout/checkout_success.html', context)
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
@@ -117,6 +199,9 @@ def checkout_success(request, order_number):
     if 'bag' in request.session:
         del request.session['bag']
 
-    context = {'order': order}
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
 
-    return render(request, 'checkout/checkout_success.html', context)
+    return render(request, template, context)
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -23,3 +24,19 @@ def profile(request):
     }
 
     return render(request, 'profiles/profile.html', context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, 'checkout/checkout_success.html', context)
