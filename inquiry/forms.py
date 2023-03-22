8e17be21a7bde46292dcf1ef309161ffa2c48d09
gozygreg <goziennabuife@yahoo.com>
2023-03-22 22:01:09 +0000

    Create inquiry form and inquiry functionality in views.py

---
+++ inquiry/forms.py
@@ -0,0 +1,41 @@
+from .models import CustomerInquiry
+from django.forms import ModelForm
+from django_summernote.widgets import SummernoteWidget
+
+
+class InquiryForm(ModelForm):
+    """
+    Form to handle inquiry form submission
+    """
+    class Meta:
+        model = CustomerInquiry
+        fields = ('name', 'email', 'subject', 'inquiry')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        """
+        Initialise the form attributes
+        """
+
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email address',
+            'subject': 'Subject',
+            'inquiry': 'Inquiry',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+
+            if self.fields[field].required:
+
+                placeholder = f'{placeholders[field]} *'
+
+            else:
+
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs[
+                'aria-label'] = placeholder
+            self.fields[field].label = False
--- inquiry/views.py
+++ inquiry/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import CreateView, View
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from .forms import InquiryForm
 
 
-class Inquiry(View):
-    def get(self, request):
-        return render(request, 'inquiry/inquiry.html')
+class ContactUs(SuccessMessageMixin, CreateView):
+    """"
+    Handles the contact form display and submission
+    """
+    form_class = InquiryForm
+    template_name = './inquiry/inquiry.html'
+    success_url = reverse_lazy('inquiry')
+    success_message = 'We have received your inquiries.\
+        We will get back to you shortly.'
+
+    def form_valid(self, form):
+        """
+        Validates form data
+        """
+
+        form.save()
+        return super().form_valid(form)
+
+
