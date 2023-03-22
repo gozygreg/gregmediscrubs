f11f735e1c1707af14f55873db0d4129ab1442cd
gozygreg <goziennabuife@yahoo.com>
2023-03-22 14:04:15 +0000

    Created submit review functionality to submit and update customer product review

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category
-from .forms import ProductForm
+from .models import Product, Category, ReviewRating
+from checkout.models import Order, OrderLineItem
+from .forms import ProductForm, ReviewForm
 
 
 def all_products(request):
@@ -129,3 +131,27 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, f'product deleted!')
     return redirect(reverse('store'))
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you. Your review is updated')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted')
+                return redirect(url)
