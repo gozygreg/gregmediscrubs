fca8a5005736de75b7c63c0413d5e63234bce426
gozygreg <goziennabuife@yahoo.com>
2023-03-22 21:50:03 +0000

    Create models for customer enquiry app and register admin

--- inquiry/admin.py
+++ inquiry/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import CustomerInquiry, InternalCommunicationNotes
 
-# Register your models here.
+
+class InternalCommunicationNotesAdmin(admin.StackedInline):
+    """
+    Internal notes for staff when replying to customers
+    messages via email
+    """
+
+    model = InternalCommunicationNotes
+    extra = 1
+    verbose_name_plural = 'Internal Communication Notes'
+    verbose_name = 'Internal Communication Note'
+    fields = ('notes', 'date',)
+    readonly_fields = ('date',)
+    can_delete = False
+
+
+@admin.register(CustomerInquiry)
+class CustomerInquiryAdmin(admin.ModelAdmin):
+    """
+    Allows staff to view/filter customer messages
+    but prevents the content being changed
+    """
+
+    inlines = (InternalCommunicationNotesAdmin,)
+    baseModel = CustomerInquiry
+    list_display = (
+        'date_received',
+        'name',
+        'email',
+        'subject',
+        'pending_reply',
+        'marked_as_done',
+    )
+    list_filter = ('pending_reply', 'marked_as_done', 'date_received')
+    search_fields = ('name', 'email', 'subject', 'inquiry',)
+    summer_note_field = ('inquiry',)
+    readonly_fields = ('date_received', 'inquiry',)
+
+    ordering = ('-date_received',)
--- inquiry/models.py
+++ inquiry/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
+class CustomerInquiry(models.Model):
+
+    date_received = models.DateTimeField(auto_now_add=True)
+
+    name = models.CharField(null=False, blank=False, max_length=50)
+
+    email = models.EmailField(null=False, blank=False, max_length=254)
+
+    subject = models.CharField(max_length=254)
+
+    inquiry = models.TextField(null=False, blank=False,)
+
+    pending_reply = models.BooleanField(default=True)
+
+    marked_as_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f" message from {self.email}"
+
+    class Meta:
+        verbose_name_plural = "Customer Inquiries"
+
+
+class InternalCommunicationNotes(models.Model):
+    """
+    Model to store internal notes for customer messages
+    """
+
+    related_inquiry = models.ForeignKey(CustomerInquiry, on_delete=models.CASCADE, related_name='related_message')
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    notes = models.TextField()
+
+    def __str__(self):
+        return f'Internal notes for message id no. {self.related_inquiry.id}'
