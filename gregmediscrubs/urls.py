0dd124d397f5b4f321025685b57561e0ba94232e
gozygreg <goziennabuife@yahoo.com>
2023-04-06 13:07:58 +0000

    Add 404.html to project

--- gregmediscrubs/urls.py
+++ gregmediscrubs/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -28,3 +30,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('inquiry/', include('inquiry.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'gregmediscrubs.views.handler404'
